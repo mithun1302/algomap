0e0bc60cc90f0d65c687cd2133c4ae9740dee1c9
Mithun Balaji <69361233+mithun1302@users.noreply.github.com>
2024-07-30 22:20:15 +0530

    Added Files

---
+++ mergeStringAlternate.java
@@ -0,0 +1,21 @@
+package com.mithun.algomap;
+
+public class mergeStringAlternate {
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+        String output = "";
+        int a=word1.length();
+        int b=word2.length();
+        int n = Math.max(a,b);
+        for(int i=0;i<n;i++){
+            if(i<a){
+                output+=word1.charAt(i);
+            }
+            if(i<b){
+                output+=word2.charAt(i);
+            }
+        }
+        System.out.println(output);
+    }
+}
