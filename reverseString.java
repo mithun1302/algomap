360a36ffaa546b52e8dae9a1ee4645cdcdfa863f
Mithun Balaji <mithunb986@gmail.com>
2024-08-09 22:36:00 +0530

    Added reverseString

---
+++ reverseString.java
@@ -0,0 +1,17 @@
+package com.mithun.algomap;
+
+public class reverseString {
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        int start = 0;
+        int end =s.length-1;
+        while(start<end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println(s);
+    }
+}
