7b53b5410111f86e25df6a3d572ef3dc2bd1f8ab
Mithun Balaji <mithunb986@gmail.com>
2024-08-11 22:51:33 +0530

    created arraysstring folder

---
+++ arrays-and-strings/containerWithMostWater.java
@@ -0,0 +1,23 @@
+package com.mithun.algomap;
+
+public class containerWithMostWater {
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int n = height.length;
+        int output = 0;
+        int left = 0;
+        int right = n-1;
+        int result = 0;
+        while(left<right){
+            int a = right - left;
+            int b = Math.min(height[left],height[right]);
+            result = Math.max(result,a*b);
+            if (height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        System.out.println(result);
+    }
+}
