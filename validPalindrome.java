ad7558a4cae956c2e96bcfc3c514d101dbb3f8f8
Mithun Balaji <mithunb986@gmail.com>
2024-08-12 22:51:16 +0530

    Added ValidPalindrome

---
+++ validPalindrome.java
@@ -0,0 +1,27 @@
+package com.mithun.algomap;
+
+public class validPalindrome {
+    public static void main(String[] args) {
+        String s = "race a car";
+        int left = 0;
+        int right = s.length()-1;
+        while (left<right){
+            int startChar = s.charAt(left);
+            int endChar = s.charAt(right);
+            if (!Character.isLetterOrDigit(startChar )) {
+                left++;
+            } else if(!Character.isLetterOrDigit(endChar)) {
+                right--;
+            }
+            else{
+                if(Character.toLowerCase(startChar)!=Character.toLowerCase(endChar)) {
+                    System.out.println("False");
+                    break;
+                }
+                left++;
+                right--;
+            }
+        }
+        System.out.println("True");
+    }
+}
